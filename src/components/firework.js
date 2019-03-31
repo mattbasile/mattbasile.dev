3a88596d4958c3931da732032380a97891350d5a
Matt Basile <matthewbasile2@gmail.com>
2019-03-31 09:30:07 -0400

    confetti working

---
+++ src/components/firework.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Confetti from 'react-dom-confetti';
+
+export default function firework(props) {
+    const config = {
+        angle: 90,
+        spread: "233",
+        startVelocity: 45,
+        elementCount: 50,
+        dragFriction: 0.1,
+        duration: 3000,
+        delay: 0,
+        width: "26px",
+        height: "34px",
+        colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+      };
+
+ return (
+    <Confetti className="absolute pin" active={ props.active } config={ config }/>
+  )
+}
+
+
--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Firework from './firework'
 
-export default function header() {
-  return (
-    <>
-      <header className="h-80 bg-blue-darker relative">
+export default class header extends Component {
+  constructor(){
+    super();
+    this.state={
+      "active":false,
+    }
+  }
+  toggleConfetti =()=>{
+    this.setState({active:true});
+    setTimeout(function(){
+      this.setState({active:false})
+    }.bind(this), 1000)
+    
+  }
+  render() {
+    return (
+      <header id="confetti-canvas" className="h-80 bg-blue-darker relative">
+      
         <div className="h-24"></div>
-        <p className="text-white text-center text-5xl pt-16">Let's build something <b className="underline">great</b> together.</p>
+        <p className="text-white text-center text-5xl pt-16">Let's build something <b onClick={e=>this.toggleConfetti()}className="underline hover:text-red cursor-pointer">great</b> together.
+        <Firework active={this.state.active}/>
+        </p>
+        
           <svg className="absolute pin-b" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1399 163">
         <path d="M1335.5,263.5" 
         transform="translate(-85 322)"
@@ -17,8 +35,8 @@ export default function header() {
         /><path d="M85-195S300-322,441-322c168,0,332,61.08,462,81,385,59,581-60,581-60v142H85Z"
         transform="translate(-85 322)" 
         style={{"fill":"white"}}/></svg>
+        
       </header>
-      
-    </>
-  )
+    )
+  }
 }
