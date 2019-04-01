3789ad21eda04e039888a2cad16160af7232134d
Matt Basile <matthewbasile2@gmail.com>
2019-03-31 20:58:23 -0400

    pulling data from medium

--- src/components/articles.js
+++ src/components/articles.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function articles() {
-  return (
-    <div>
-      <h1>articles</h1>
-    </div>
-  )
+export default class articles extends Component {
+  constructor(){
+    super();
+    this.state={
+      articles: []
+    }
+  }
+  
+  
+  componentDidMount(){
+    axios.get("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40mattbasile")
+    .then(res=> this.setState({articles: res.data.items}))
+    .catch(err=>console.log(err))
+  }
+  render() {
+    return (
+      <>
+
+    {
+      this.state.articles.map(article =>{
+        if(article.categories.length > 0){
+          return <div>{article.title}</div> 
+        }
+      })
+      }
+      </>
+    )
+  }
 }
