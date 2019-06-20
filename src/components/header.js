db9e945900c3c926e6368a3186bc423fd159c2df
Matt Basile <matthewbasile2@gmail.com>
2019-06-20 10:03:58 -0400

    fixing border one mobile

--- src/components/header.js
+++ src/components/header.js
@@ -21,20 +21,20 @@ export default class header extends Component {
       
         <div className="hidden md:block md:h-24"></div>
         <p className="flex flex-wrap justify-center text-white text-center title-size pt-6 md:pt-16">Let's build something <b onClick={e=>this.toggleConfetti()}className="app_confetti underline hover:text-red-600 cursor-pointer px-3">great <Firework active={this.state.active}/></b> together.
-        
+  
         </p>
         
         <svg className="absolute bottom-0 border-0" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1399 163">
         <path d="M1335.5,263.5" 
         transform="translate(-85 322)"
-        style={{"fill":"#EBF8FF", "stroke":"#EBF8FF", "strokeMiterlimit":10}}
+        style={{"fill":"#EBF8FF", }}
         />
         <path d="M512.5,99.5"
         transform="translate(-85 322)"
-        style={{"fill":"#EBF8FF","stroke":"#EBF8FF", "strokeMiterlimit":10}}
+        style={{"fill":"#EBF8FF", }}
         /><path d="M85-195S300-322,441-322c168,0,332,61.08,462,81,385,59,581-60,581-60v142H85Z"
         transform="translate(-85 322)" 
-        style={{"fill":"#EBF8FF", "stroke":"#EBF8FF"}}/></svg>
+        style={{"fill":"#EBF8FF", }}/></svg>
         
       </header>
     )
